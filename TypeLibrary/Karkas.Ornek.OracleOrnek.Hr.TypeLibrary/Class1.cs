abcb5cea411306a360095fee3667bbe11d615ea0
Atilla Ozgur <ati.ozgur@gmail.com>
2013-04-27 00:00:32 +0300

    TypeLibrary commit

---
+++ TypeLibrary/Karkas.Ornek.OracleOrnek.Hr.TypeLibrary/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Karkas.Ornek.OracleOrnek.Hr.TypeLibrary
+{
+    public class Class1
+    {
+    }
+}
